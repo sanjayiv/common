d4014ebc6e69350b3275884a6e1188c1f16a8ce8
Sanjay Vaghela <Sanjay_Vaghela@mckinsey.com>
2014-06-08 20:35:41 +0530

    default output & logger through utils

--- stub.py
+++ stub.py
@@ -1,10 +1,15 @@
 import sys
 import optparse
 
+from utils import formatted_filepath, get_logger
+
 def parse_args():
+    default_output = formatted_filepath('output', datestamp=True)
+    default_logger = formatted_filepath(suffix='log', sep='.')
     parser = optparse.OptionParser()
     parser.add_option("-i", "--input", default=None, help="Input")
-    parser.add_option("-o", "--output", default=None, help="Output")
+    parser.add_option("-o", "--output", default=default_output, help="Output name")
+    parser.add_option("-l", "--logger", default=default_logger, help="Log name")
     (options, args) = parser.parse_args()
     if not (options.input and options.output):
         parser.print_help()
@@ -14,6 +19,8 @@ def parse_args():
 if __name__ == '__main__':
     try:
         options, args = parse_args()
+        print options.input, options.output, options.logger
+        logger = get_logger(options.logger)
     except SystemExit, ee:
         if 1 == ee.code:
             print "Error: Mandatory arguments missing!!"
